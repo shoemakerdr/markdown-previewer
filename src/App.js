236b4930fce871fd19b9bfbe6fa144395c15b92c
Derek Shoemaker <shoemakerdr@gmail.com>
2017-08-20 15:26:01 -0500

    Replaced boilerplace create-react-app code with working markdown previewer code. Added styling and initial state.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ReactMarkdown from 'react-markdown'
+
+const initialState = {
+    input: '## Preview Window\n**This** is where your *markdown* text will be:\n\n * rendered \n* as \n* html'
+        }
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor () {
+        super()
+        this.setInput = this.setInput.bind(this)
+        this.state = initialState
+    }
+
+    setInput (event) {
+        const value = event.target.value === '' ? initialState.input : event.target.value
+        this.setState({
+            input: value 
+        })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className='App-header'>
+                    <h1>Markdown Previewer</h1>
+                </div>
+                <div className='inputOutput'>
+                    <textarea className='input' placeholder='Type markdown here to be rendered as HTML' type='text' onInput={this.setInput}></textarea>
+                    <ReactMarkdown className='ReactMarkdown' source={this.state.input} />
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
